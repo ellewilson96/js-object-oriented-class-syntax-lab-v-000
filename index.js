85e4e2a3f18ee2cc617a4f483e4237af88da60c0
Elle Wilson <eleanorwilsonn@gmail.com>
2018-08-27 16:35:11 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+class BoardMember {
+  constructor(name, homeState, training){
+  this.name = name
+  this.homeState = homeState
+  this.training = training
+  }
+}
+
+  BoardMember.veto = function(){
+    return 'No, I must disagree'
+  }
+  BoardMember.approve = function(){
+    return 'You can do that!'
+  }
+  BoardMember.doCharity = function(){
+    return "I like to help people."
+  }
+
+  BoardMember.releasePressStatement = function(){
+    return "You will see great things from Scuber."
+  }
+
+  BoardMember.sayHi = function(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  }
