47284ecdda679b32aeed7ba5bcda986e0c0e1dd3
Elle Wilson <eleanorwilsonn@gmail.com>
2018-08-27 16:39:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,11 +27,6 @@ class BoardMember {
 }
 
 class Ceo extends BoardMember {
-  constructor(name, training, homeState){
-    this.name = name;
-    this.homeState = homeState;
-    this.training = training;
-  }
 
   hireEmployee() {
     return "Welcome aboard!"
